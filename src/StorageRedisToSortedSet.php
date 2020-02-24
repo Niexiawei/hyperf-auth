ad4d918d199871dc79e6a37b733b69d74a654a9b
Niexiawei <1135215907@qq.com>
2020-02-24 15:59:39 +0800

    Update StorageRedisToSortedSet.php

--- src/StorageRedisToSortedSet.php
+++ src/StorageRedisToSortedSet.php
@@ -23,142 +23,264 @@ class StorageRedisToSortedSet implements StorageInterface
     /**
      * @Inject()
      * @var RedisFactory
-
      */
     protected $RedisFactory;
     protected $user_token_list;
+
     public function __construct(ContainerInterface $container)
     {
-        $this->user_token_list = env('APP_NAME').'_user_token_list';
+        $this->user_token_list = env('APP_NAME','hyperf') . '_user_token_list';
     }
-    public function config($key,$default = ''){
-        return $this->configInterface->get('auth.'.$key,$default);
+
+    /**
+     * @param $key
+     * @param string $default
+     * @return mixed
+     * 获取配置文件
+     */
+
+    public function config($key, $default = '')
+    {
+        return $this->configInterface->get('auth.' . $key, $default);
     }
+
+    /**
+     * @return \Hyperf\Redis\RedisProxy|\Redis
+     * 获取Redis
+     */
+
     protected function redis()
     {
-        $pool = $this->config('redis_pool','default');
+        $pool = $this->config('redis_pool', 'default');
         return $this->RedisFactory->get($pool);
     }
-    private function tokenKey(string $token){
+
+    /**
+     * @param string $token
+     * @return string
+     * @throws Exception
+     * 根据token获取集合中的key
+     */
+
+    private function tokenKey(string $token)
+    {
         $origin = $this->unFormat($token);
-        $key = $origin['guard'].'_'.$origin['uid'].'_'.$token.'_'.$origin['create_time'];
+        $key = $origin['guard'] . '_' . $origin['uid'] . '_' . $token . '_' . $origin['create_time'];
         return $key;
     }
-    protected function tokenSave(string $token){
+
+    /**
+     * @param string $token
+     * @throws Exception
+     * 保存 token
+     */
+
+    protected function tokenSave(string $token)
+    {
         $this->delSurplusToken($token);
         $key = $this->tokenKey($token);
         $expire = $this->getTTL();
         $expire_timestamp = Carbon::now()->addSeconds($expire)->getTimestamp();
-        $this->redis()->zAdd($this->user_token_list,[],$expire_timestamp,$key);
+        $this->redis()->zAdd($this->user_token_list, [], $expire_timestamp, $key);
     }
-    public function tokenExists(string $token):int
+
+    /**
+     * @param string $token
+     * @return int
+     * @throws Exception
+     * 检查token是否存在 存在返回 token 过期时间
+     */
+
+    public function tokenExists(string $token): int
     {
         $key = $this->tokenKey($token);
-        $expire = $this->redis()->zScore($this->user_token_list,$key);
-        if(empty($expire)){
+        $expire = $this->redis()->zScore($this->user_token_list, $key);
+        if (empty($expire)) {
             return 0;
         }
         return $expire;
     }
-    protected function getUidTokens(string $token){
+
+    /**
+     * @param string $token
+     * @return array
+     * @throws Exception
+     * 获取 当前用户的 所有token
+     */
+    protected function getUidTokens(string $token)
+    {
         $tokens = [];
         $now = Carbon::now()->getTimestamp();
         $origin = $this->unFormat($token);
-        $search = $origin['guard'].'_'.$origin['uid'].'_*';
+        $search = $origin['guard'] . '_' . $origin['uid'] . '_*';
         $it = null;
-        $del_token = [];
-        while (true){
-            $arr = $this->redis()->zScan($this->user_token_list,$it,$search);
-            if($arr === false){
+        $del_expire_token = [];
+        while (true) {
+            $arr = $this->redis()->zScan($this->user_token_list, $it, $search);
+            if ($arr === false) {
                 break;
             }
-            foreach ($arr as $token_key => $expire){
-                if($now > $expire){
-                    $del_token[] = $token_key;
-                }else{
-                    $tokens[] = ['token_key'=>$token_key,'expire'=>$expire];
+            foreach ($arr as $token_key => $expire) {
+                if ($now > $expire) {
+                    $del_expire_token[] = $token_key;
+                } else {
+                    $tokens[] = ['token_key' => $token_key, 'expire' => $expire];
                 }
             }
         }
-        if(!empty($del_token)){
-            $this->delToken(implode(',',$del_token));
+        if (!empty($del_expire_token)) {
+            $this->redis()->multi(\Redis::PIPELINE);
+            foreach ($del_expire_token as $token) {
+                $this->delToken($token);
+            }
+            $this->redis()->exec();
         }
         return $tokens;
     }
-    private function delToken($token){
-        $this->redis()->zRem($this->user_token_list,$token);
+
+    /**
+     * @param $token
+     * 根据token_key 删除token
+     */
+
+    private function delToken($token)
+    {
+        $this->redis()->zRem($this->user_token_list, $token);
     }
+
+
+    /**
+     * @param string $guard
+     * @param int $uid
+     * @return string
+     * @throws Exception
+     * 生成 token
+     */
+
     public function generate(string $guard, int $uid)
     {
         $data = [
-            'guard'=>$guard,
-            'uid'=>$uid,
-            'create_time'=>time(),
-            'str'=>Str::random(32),
+            'guard' => $guard,
+            'uid' => $uid,
+            'create_time' => time(),
+            'str' => Str::random(32),
             'allow_refresh_token' => $this->getAllowRefreshToken() ? 1 : 0
         ];
         $token = $this->format($data);
         $this->tokenSave($token);
         return $token;
     }
-    private function format(array $data){
+
+    /**
+     * @param array $data
+     * @return string
+     * 格式化 token
+     */
+
+    private function format(array $data)
+    {
         $key = $this->config('key');
         $token_head = base64_encode(json_encode($data));
-        $token_sign = md5($token_head.$key);
-        return $token_head.'.'.$token_sign;
+        $token_sign = md5($token_head . $key);
+        return $token_head . '.' . $token_sign;
     }
-    private function unFormat(string $token){
+
+
+    /**
+     * @param string $token
+     * @return array|mixed
+     * @throws Exception
+     * 解析token
+     */
+
+    private function unFormat(string $token)
+    {
         $origin = $this->tokenToOrigin($token);
-        if(!empty($origin)){
+        if (!empty($origin)) {
             return $origin;
-        }else{
-            $origin = explode('.',$token);
-            if(isset($origin[0]) && isset($origin[1])){
+        } else {
+            $origin = explode('.', $token);
+            if (isset($origin[0]) && isset($origin[1])) {
                 $token_head = $origin[0];
                 $token_sign = $origin[1];
-                $origin_token = json_decode(base64_decode($token_head),true);
+                $origin_token = json_decode(base64_decode($token_head), true);
                 $origin_token['sign'] = $token_sign;
-                $this->tokenToOriginCache($token,$origin_token);
+                $this->tokenToOriginCache($token, $origin_token);
                 return $origin_token;
             }
         }
         throw new Exception('Token无法解析');
     }
+
+    /**
+     * @return bool
+     * 获取 token 是否可以 刷新
+     */
+
     private function getAllowRefreshToken(): bool
     {
-        $allow = Context::get(AllowRefreshOrNotInterface::class,true);
-        return  $allow;
+        $allow = Context::get(AllowRefreshOrNotInterface::class, true);
+        return $allow;
     }
-    public function delete($token){
+
+    /**
+     * @param $token
+     * @throws Exception
+     * 删除token  退出登录用
+     */
+
+    public function delete($token)
+    {
         $keys = $this->tokenKey($token);
-        $this->redis()->zRem($this->user_token_list,$keys);
+        $this->redis()->zRem($this->user_token_list, $keys);
     }
+
+    /**
+     * @return mixed|setTokenExpireInterface|null
+     * 获取 token过期时间
+     */
+
     private function getTTL()
     {
         $config = $this->config('expire', 3600 * 24);
-        $second = Context::get(setTokenExpireInterface::class,$config);
+        $second = Context::get(setTokenExpireInterface::class, $config);
         return $second;
     }
+
+    /**
+     * @param $token
+     * @throws Exception
+     * 删除 超出最大登录的token
+     */
+
     private function delSurplusToken($token)
     {
         $tokens = $this->getUidTokens($token);
         $num = count($tokens);
-        $max_num = $this->config('max_login_num',7) - 1;
-        if($num > $max_num){
+        $max_num = $this->config('max_login_num', 7) - 1;
+        if ($num > $max_num) {
             $delNum = $num - $max_num;
-            array_multisort(array_column($tokens,'expire'),SORT_ASC,$tokens);
-            $delTokens = array_splice($tokens,0,$delNum);
-            $del_token_arr = array_map(function ($tokens){
+            array_multisort(array_column($tokens, 'expire'), SORT_ASC, $tokens);
+            $delTokens = array_splice($tokens, 0, $delNum);
+            $del_token_arr = array_map(function ($tokens) {
                 return $tokens['token_key'];
-            },$delTokens);
-            if(!empty($del_token_arr)){
-                var_dump($del_token_arr);
-                foreach ($del_token_arr as $token){
+            }, $delTokens);
+            if (!empty($del_token_arr)) {
+                $this->redis()->multi(\Redis::PIPELINE);
+                foreach ($del_token_arr as $token) {
                     $this->delToken($token);
                 }
+                $this->redis()->exec();
             }
         }
     }
+
+
+    /**
+     * @param $token
+     * @throws Exception
+     * 刷新token
+     */
     public function refresh($token)
     {
         $origin = $this->unFormat($token);
@@ -166,32 +288,64 @@ class StorageRedisToSortedSet implements StorageInterface
         $token_key = $this->tokenKey($token);
         $create_time = Carbon::createFromTimestamp($origin['create_time']);
         $expire_time = Carbon::createFromTimestamp($expire_timestamp);
-        $max_login_time = $this->config('max_login_time',3600 * 24);
-        if($create_time->diffInSeconds($create_time) <= $max_login_time){
+        $max_login_time = $this->config('max_login_time', 3600 * 24);
+        if ($create_time->diffInSeconds($create_time) <= $max_login_time) {
             $expire = $expire_time->addSeconds($this->getTTL())->getTimestamp();
-            $this->redis()->zAdd($this->user_token_list,[],$expire,$token_key);
+            $this->redis()->zAdd($this->user_token_list, [], $expire, $token_key);
         }
     }
+
+    /**
+     * @param $token
+     * @return array
+     * @throws Exception
+     * 验证token
+     */
+
     public function verify($token): array
     {
+        $this->getUidTokens($token);
         $expire = $this->tokenExists($token);
-        if($expire <= 0){
-            return  [];
+        if ($expire <= 0 || Carbon::now()->getTimestamp() > $expire) {
+            return [];
         }
+        $this->refresh($token);
         return $this->unFormat($token);
     }
+
+    /**
+     * @param $token
+     * @return array|mixed
+     * @throws Exception
+     * 格式化token
+     */
+
     public function formatToken($token)
     {
         return $this->unFormat($token);
     }
 
-    private function tokenToOriginCache($token,$orgin){
-        $token_cache = Context::get('tokens_cache',[]);
-        Context::set('tokens_cache',array_merge($token_cache,[$token=>$orgin]));
+    /**
+     * @param $token
+     * @param $orgin
+     * 缓存
+     */
+
+    private function tokenToOriginCache($token, $orgin)
+    {
+        $token_cache = Context::get('tokens_cache', []);
+        Context::set('tokens_cache', array_merge($token_cache, [$token => $orgin]));
     }
 
-    private function tokenToOrigin($token){
-        $token_cache = Context::get('tokens_cache',[]);
+    /**
+     * @param $token
+     * @return array
+     * 获取缓存
+     */
+
+    private function tokenToOrigin($token)
+    {
+        $token_cache = Context::get('tokens_cache', []);
         return $token_cache[$token] ?? [];
     }
 }
