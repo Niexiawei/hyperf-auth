f782028b176a8347fa774ae375381d44c96d588f
Niexiawei <1135215907@qq.com>
2020-06-12 14:48:34 +0800

    no message

--- src/DriveInterface.php
+++ src/DriveInterface.php
@@ -8,6 +8,6 @@ interface DriveInterface
 {
     public function generate(string $guard,int $uid);
     public function delete($token);
-    public function refresh($token);
+    public function refresh(AuthUserObj $userObj);
     public function verify($token):AuthUserObj;
 }
