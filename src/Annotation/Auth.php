5b9b1b81163b654672afa63d2e53f0961dca276f
Niexiawei <1135215907@qq.com>
2021-10-29 14:51:42 +0800

    Update Auth.php

--- src/Annotation/Auth.php
+++ src/Annotation/Auth.php
@@ -5,11 +5,12 @@ namespace Niexiawei\Auth\Annotation;
 
 use Attribute;
 use Hyperf\Di\Annotation\AbstractAnnotation;
+
 /**
  * @Annotation()
  * @Target({"CLASS", "METHOD"})
  */
-#[Attribute(Attribute::TARGET_METHOD,Attribute::TARGET_CLASS)]
+#[Attribute(Attribute::TARGET_METHOD | Attribute::TARGET_CLASS)]
 class Auth extends AbstractAnnotation
 {
 
