5f960dfb5251db95ca7290836a5adc8ab953626a
Niexiawei <1135215907@qq.com>
2022-12-06 14:32:34 +0800

    如果key 不存在 在第一次启动时候会自动生成

--- src/Listener/InitAuthKeyListener.php
+++ src/Listener/InitAuthKeyListener.php
@@ -9,6 +9,7 @@ use Hyperf\Contract\StdoutLoggerInterface;
 use Hyperf\Event\Contract\ListenerInterface;
 use Hyperf\Framework\Event\BeforeMainServerStart;
 use Hyperf\Utils\ApplicationContext;
+use Hyperf\Utils\Str as HyperfUtilsStr;
 
 class InitAuthKeyListener implements ListenerInterface
 {
@@ -26,12 +27,20 @@ class InitAuthKeyListener implements ListenerInterface
             $config = $app->get(ConfigInterface::class);
 
             if (!file_exists(BASE_PATH . '/auth_key')) {
-                $app->get(StdoutLoggerInterface::class)->error("auth_key文件不存在，使用命令(generate:auth_key)生成auth_key文件不存在文件。");
-                return;
+                $this->generateAuthKeyFile($config);
+            } else {
+                $key = file_get_contents(BASE_PATH . '/auth_key');
+                $config->set('auth.key', $key);
             }
-
-            $key = file_get_contents(BASE_PATH . '/auth_key');
-            $config->set('auth.key', $key);
         }
+    }
+
+    private function generateAuthKeyFile(ConfigInterface $config)
+    {
+        $key = HyperfUtilsStr::random(32);
+        $f = fopen(BASE_PATH . '/auth_key', 'w+');
+        fwrite($f, $key);
+        fclose($f);
+        $config->set('auth.key', $key);
     }
 }
